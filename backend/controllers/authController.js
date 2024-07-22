fd167507f3bb4cea27a8b14b2f6cbef3ba66320a
mohanned-sameh <mohanned2678@gmail.com>
2024-07-22 22:03:50 +0300

    ```text refactor: Set default role to 'user' in authController.js
    This commit updates the authController.js file to set the default role to 'user' when registering a new user. This change ensures that all new users are assigned the 'user' role by default, even if the role parameter is not provided.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -18,7 +18,7 @@ exports.register = async (req, res) => {
       email,
       password: hashedPassword,
       address,
-      role: role || 'user',
+      role: 'user',
     });
 
     const token = generateToken(user);
