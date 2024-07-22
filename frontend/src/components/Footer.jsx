a3a565d126472cfea972c102bc97974f68d725a8
mohanned-sameh <mohanned2678@gmail.com>
2024-07-22 17:09:45 +0300

    ```text refactor: Update Footer component to optimize rendering of category images
    This commit refactors the Footer component to optimize the rendering of category images. It replaces the previous implementation with a more efficient approach that uses a single grid container for all categories, instead of creating a separate grid container for each category. This change improves the performance of the page by reducing the number of DOM elements and enhancing the rendering speed.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,29 +3,27 @@ const Footer = ({ categories }) => {
     <footer className='bg-gray-800 text-white mt-auto'>
       <div className='container mx-auto px-4 py-16'>
         <h2 className='text-3xl font-bold mb-8'>Featured Categories</h2>
-        <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
-          {categories.map((category) => (
-            <>
-              {category.id >= 5 ? null : (
-                <div
-                  key={category.id}
-                  className='bg-gray-700 rounded-lg shadow-md overflow-hidden'
-                >
-                  <img
-                    src={`http://localhost:3000${category.image}`}
-                    alt={category.name}
-                    className='w-full h-48 object-cover'
-                  />
-                  <div className='p-4'>
-                    <h3 className='font-semibold text-lg text-white'>
-                      {category.name}
-                    </h3>
-                  </div>
+        {categories.map((category) => (
+          <div
+            key={category.id}
+            className='grid grid-cols-2 md:grid-cols-4 gap-4'
+          >
+            {category.id >= 5 ? null : (
+              <div className='bg-gray-700 rounded-lg shadow-md overflow-hidden'>
+                <img
+                  src={`http://localhost:3000${category.image}`}
+                  alt={category.name}
+                  className='w-full h-48 object-cover'
+                />
+                <div className='p-4'>
+                  <h3 className='font-semibold text-lg text-white'>
+                    {category.name}
+                  </h3>
                 </div>
-              )}
-            </>
-          ))}
-        </div>
+              </div>
+            )}
+          </div>
+        ))}
       </div>
       <div className='container mx-auto text-center py-4'>
         &copy; {new Date().getFullYear()} Tech-Store
